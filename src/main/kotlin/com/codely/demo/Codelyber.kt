2f2caff3d2f6654fe79098d30b86e75b9b8bf512
sefhirot69 <josalillo@gmail.com>
2022-12-11 19:48:02 +0100

    Feat: calculate year old

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,9 +1,23 @@
 package com.codely.demo
 
 import java.time.LocalDate
+import java.time.Period
+import kotlin.system.exitProcess
 
 fun main() {
     println("Please enter a date with the format <yyyy-MM-dd>")
-    var input = LocalDate.parse(readLine())
-    println("You wrote $input ${input.dayOfWeek}")
+    val line = supportNullableString(readLine())
+
+    if(line.isNullOrEmpty()) {
+        println("The date is not valid")
+        exitProcess(1);
+    }
+    val input = LocalDate.parse(line)
+    println("You wrote $input")
+    val currentDate = LocalDate.now()
+    val difference = Period.between(input, currentDate)
+    println("The difference between the date you wrote and today is ${difference.years}")
+
 }
+
+fun supportNullableString(line: String?) = line
